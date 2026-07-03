c546eca888fd8c0a5ec272361f8813391a766ec6
vandana-pathak12 <vandanapathak1212@gmail.com>
2026-07-03 15:06:14 +0530

    Add new code

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,18 @@
-console.log('My first JavaScript file is connected!');
-alert('Welcome to JavaScript, ' + 'vandana pathak');
+// console.log('My first JavaScript file is connected!');
+// alert('Welcome to JavaScript, ' + 'vandana pathak');
+
+const fullName = "Vandana Pathak"; // string
+let age = 19; // number
+const isStudent = true; // boolean
+let favoriteColor; // undefined
+const pet = null; // null
+
+console.log(`Name: ${fullName}, type: ${typeof fullName}`);
+
+console.log(`Age: ${age}, type: ${typeof age}`);
+
+console.log(`Is Student: ${isStudent}, type: ${typeof isStudent}`);
+
+console.log(`Favorite Color: ${favoriteColor}, type: ${typeof favoriteColor}`);
+
+console.log(`Pet: ${pet}, type: ${typeof pet}`);
