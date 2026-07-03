0c8a5dd64a97909d3bab2dbd7ccd3eeeb22ce6c2
vandana-pathak12 <vandanapathak1212@gmail.com>
2026-07-03 15:07:10 +0530

    add code

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,28 @@
 // console.log('My first JavaScript file is connected!');
 // alert('Welcome to JavaScript, ' + 'vandana pathak');
 
-const fullName = "Vandana Pathak"; // string
-let age = 19; // number
-const isStudent = true; // boolean
-let favoriteColor; // undefined
-const pet = null; // null
+// const fullName = "Vandana Pathak"; // string
+// let age = 19; // number
+// const isStudent = true; // boolean
+// let favoriteColor; // undefined
+// const pet = null; // null
 
-console.log(`Name: ${fullName}, type: ${typeof fullName}`);
+// console.log(`Name: ${fullName}, type: ${typeof fullName}`);
 
-console.log(`Age: ${age}, type: ${typeof age}`);
+// console.log(`Age: ${age}, type: ${typeof age}`);
 
-console.log(`Is Student: ${isStudent}, type: ${typeof isStudent}`);
+// console.log(`Is Student: ${isStudent}, type: ${typeof isStudent}`);
 
-console.log(`Favorite Color: ${favoriteColor}, type: ${typeof favoriteColor}`);
+// console.log(`Favorite Color: ${favoriteColor}, type: ${typeof favoriteColor}`);
 
-console.log(`Pet: ${pet}, type: ${typeof pet}`);
+// console.log(`Pet: ${pet}, type: ${typeof pet}`);
+
+
+const num1 = 25;
+const num2 = 4;
+console.log(`Sum: ${num1 + num2}`);
+console.log(`Difference: ${num1 - num2}`);
+console.log(`Product: ${num1 * num2}`);
+console.log(`Quotient: ${num1 / num2}`);
+console.log(`Remainder: ${num1 % num2}`);
+console.log(`Power: ${num1 ** 2}`);
